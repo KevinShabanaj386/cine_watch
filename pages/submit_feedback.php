b0943494b11c33f37956b1949d0282a699aaf04c
Dea343 <dea343>
2025-01-19 21:53:01 +0100

    commit7

--- pages/submit_feedback.php
+++ pages/submit_feedback.php
@@ -29,6 +29,9 @@ if ($stmt->execute()) {
 } else {
     echo "Error: " . $stmt->error;
 }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Close connections
 $stmt->close();
