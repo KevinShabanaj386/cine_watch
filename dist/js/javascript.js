035b4524dc4e3acd29a698ab3d6252dc1d869fc3
Dea343 <dea34377@gmail.com>
2025-01-30 00:34:57 +0100

    commit9

--- dist/js/javascript.js
+++ dist/js/javascript.js
@@ -3,11 +3,11 @@ function toggleVisibility(section) {
     const surveySection = document.getElementById('survey');
     const contactSection = document.getElementById('contact');
 
-    // Hide all sections first
+    
     surveySection.classList.add('hidden');
     contactSection.classList.add('hidden');
 
-    // Show the selected section
+    
     if (section === 'support') {
         surveySection.classList.remove('hidden');
     } else if (section === 'contact') {
